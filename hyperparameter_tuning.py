6c65ae7902bef260a43df1a997859cc82b6d1eda
2022ac05070 <2022ac05070@wilp.bits-pilani.ac.in>
2024-08-03 10:55:01 +0530

    Update hyperparameter_tuning.py

--- hyperparameter_tuning.py
+++ hyperparameter_tuning.py
@@ -25,7 +25,6 @@ grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=3, scoring
 with mlflow.start_run() as run:
     # Fit the model
     grid_search.fit(X, y)
-    
     # Get the best parameters
     best_params = grid_search.best_params_
     best_score = grid_search.best_score_
